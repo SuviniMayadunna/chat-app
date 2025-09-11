5767687e4cd927d77f6240bce1cdc227cf590fa1
SuviniMayadunna <mayadunnasj.22@uom.lk>
2025-09-11 10:36:38 +0530

    Messages, and Input components ,include image assets

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,9 +2,30 @@ import React from "react";
 
 const Chats = () => {
   return (
-  <div>
-    
+  <div className="chats">
+    <div className="userChat">
+      <img src="https://i.pinimg.com/736x/ae/e6/d4/aee6d45245609592339c8508ae27182d.jpg"/>
+      <div className="userChatInfo">
+        <span>Meyon</span>
+        <p>Hello</p>
+      </div>
+    </div>
+    <div className="userChat">
+      <img src="https://i.pinimg.com/736x/ae/e6/d4/aee6d45245609592339c8508ae27182d.jpg"/>
+      <div className="userChatInfo">
+        <span>Meyon</span>
+        <p>Hello</p>
+      </div>
+    </div>
+    <div className="userChat">
+      <img src="https://i.pinimg.com/736x/ae/e6/d4/aee6d45245609592339c8508ae27182d.jpg"/>
+      <div className="userChatInfo">
+        <span>Meyon</span>
+        <p>Hello</p>
+      </div>
+    </div>
   </div>
+  
 )
 }
 export default Chats;
